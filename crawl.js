0ea561078f6a4ac30a34dd18708b17feb7dfd393
Jordi Noguera <jordinoguera83@gmail.com>
2024-06-19 11:04:23 +0200

    Add superagent

--- crawl.js
+++ crawl.js
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import { PromisePool } from '@supercharge/promise-pool'
+import superagent from 'superagent'
 
 const file = readFileSync('../jobanni/data/top-1000.txt', 'utf8')
 
@@ -7,19 +8,25 @@ const urls = file.split(/\n/)
 
 const start = new Date()
 
+
+const makeRequest = async url => {
+    try {
+        const response = await superagent.get(url)
+            .timeout(10000)
+            .set('User-Agent', 'crawler-test')
+            .ok(() => true)
+        console.error(`${url}: ${response.status}`)
+        return response.status
+    } catch (error) {
+        console.error(`${url}: ${error.code} ${error.errno}`)
+        return error.code || 'error'
+    }
+}
+
 const { results, errors } = await PromisePool
     .for(urls)
-    .withConcurrency(20)
-    .process(async url => {
-        try {
-            const response = await fetch(url, {signal: AbortSignal.timeout(10000)})
-            console.log(`${url}: ${response.status}`)
-            return response.status
-        } catch (error) {
-            console.error(`${url}: ${error.cause && error.cause.toString() || error.toString()}`)
-            return 'error'
-        }
-    })
+    .withConcurrency(50)
+    .process(makeRequest)
 
 const aggregates = results.reduce((agg, number) => {
     return { ...agg, [number]: (agg[number] || 0) + 1 }
