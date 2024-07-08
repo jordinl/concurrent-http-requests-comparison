8761f3df69ccab157439fbe5c34aef4fe709b192
Jordi Noguera <jordinoguera83@gmail.com>
2024-07-08 08:30:21 +0200

    Add Java

---
+++ java/http/main.java
@@ -0,0 +1,147 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+class Result {
+    private String code;
+    private long time;
+
+    public Result(String code, long time) {
+        this.code = code;
+        this.time = time;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public long getTime() {
+        return time;
+    }
+}
+
+class Main {
+    private static final Integer LIMIT = getEnv("LIMIT", 1000);
+    private static final Integer CONCURRENCY = getEnv("CONCURRENCY", 10);
+    private static final Integer REQUEST_TIMEOUT = getEnv("REQUEST_TIMEOUT", 5);
+    private static final String FILE_PATH = "/mnt/appdata/urls.txt";
+    public static CountDownLatch latch = new CountDownLatch(LIMIT);
+    public static Semaphore semaphore = new Semaphore(CONCURRENCY);
+
+    private static final HttpClient CLIENT = HttpClient.newBuilder()
+            .followRedirects(HttpClient.Redirect.ALWAYS)
+            .build();
+
+    private static HttpRequest buildHttpRequest(String url) {
+        return HttpRequest.newBuilder()
+                          .uri(URI.create(url))
+                          .header("User-Agent", "crawler-test")
+                          .header("Accept-Encoding", "gzip, deflate, br")
+                          .timeout(Duration.ofSeconds(REQUEST_TIMEOUT))
+                          .GET()
+                          .build();
+    }
+
+    private static Integer getEnv(String variableName, Integer defaultValue) {
+        String value = System.getenv(variableName);
+        return value != null ? Integer.parseInt(value) : defaultValue;
+    }
+
+    public static Result makeRequest(String url) {
+        long startTime = System.currentTimeMillis();
+        String code = "";
+        CompletableFuture<HttpResponse<Void>> future = null;
+
+        try {
+            HttpRequest request = buildHttpRequest(url);
+            future = CLIENT.sendAsync(request, HttpResponse.BodyHandlers.discarding());
+            HttpResponse<Void> response = future.get(REQUEST_TIMEOUT, TimeUnit.SECONDS);
+            code = Integer.toString(response.statusCode());
+        } catch (TimeoutException e) {
+            future.cancel(true);
+            code = "TimeoutException";
+        } catch (Exception e) {
+            code = e.getClass().getSimpleName();
+        } finally {
+            semaphore.release();
+            latch.countDown();
+        }
+
+        long endTime = System.currentTimeMillis();
+        long requestTime = endTime - startTime;
+        System.out.println("URL " + url + " -- Code: " + code + " -- Request Time: " + requestTime + "ms");
+        return new Result(code, requestTime);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        long start = System.currentTimeMillis();
+
+        System.out.println("Starting");
+        System.out.println(" * LIMIT: " + LIMIT);
+        System.out.println(" * CONCURRENCY: " + CONCURRENCY);
+        System.out.println(" * REQUEST_TIMEOUT: " + REQUEST_TIMEOUT);
+
+        File file = new File(FILE_PATH);
+        Scanner reader = new Scanner(file);
+
+        List<Result> results = new ArrayList<>();
+
+        Integer count = 0;
+        while (reader.hasNextLine() && count < LIMIT) {
+            String url = reader.nextLine();
+            semaphore.acquire();
+            Thread.startVirtualThread(() -> {
+                Result result = makeRequest(url);
+                synchronized (results) {
+                    results.add(result);
+                }
+            });
+            count++;
+        }
+
+        reader.close();
+
+        // Wait for all virtual threads to complete
+        latch.await();
+
+        Map<String, Long> aggregates = results.stream()
+                .collect(Collectors.groupingBy(Result::getCode, Collectors.counting()));
+
+        double avgTime = results.stream()
+                .mapToLong(Result::getTime)
+                .average()
+                .orElse(0);
+
+        List<Long> times = results.stream()
+                .map(Result::getTime)
+                .sorted()
+                .collect(Collectors.toList());
+        long medianTime = times.get(times.size() / 2);
+        long maxTime = times.stream()
+                .max(Long::compareTo)
+                .orElse(0L);
+
+        long totalTime = (System.currentTimeMillis() - start) / 1000;
+        long totalUrls = aggregates.values().stream().mapToLong(Long::longValue).sum();
+
+        System.out.println("Total time: " + totalTime + "s");
+        System.out.println("Average time: " + avgTime);
+        System.out.println("Median time: " + medianTime);
+        System.out.println("Max time: " + maxTime);
+        System.out.println("Total URLs: " + totalUrls);
+        System.out.println(aggregates);
+    }
+}
