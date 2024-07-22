4095c08ccfde2351103a6b41b55ac3680422d237
Jordi Noguera <jordinoguera83@gmail.com>
2024-07-22 19:38:13 +0200

    Try semaphore

--- deno/fetch/deps.js
+++ deno/fetch/deps.js
@@ -1,2 +1,3 @@
 export { TextLineStream } from "https://deno.land/std@0.224.0/streams/text_line_stream.ts";
 export { PromisePool } from "npm:@supercharge/promise-pool@3.2.0";
+export {Sema as Semaphore} from "npm:async-sema";
