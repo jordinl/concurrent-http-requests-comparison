935078b67caee2e87e43111c467939eab6a84c8c
Jordi Noguera <jordinoguera83@gmail.com>
2024-07-24 11:52:30 +0200

    Revert "Try semaphore"
    This reverts commit 4095c08ccfde2351103a6b41b55ac3680422d237.

--- deno/fetch/deps.js
+++ deno/fetch/deps.js
@@ -1,3 +1,2 @@
 export { TextLineStream } from "https://deno.land/std@0.224.0/streams/text_line_stream.ts";
 export { PromisePool } from "npm:@supercharge/promise-pool@3.2.0";
-export {Sema as Semaphore} from "npm:async-sema";
--- deno/fetch/main.js
+++ deno/fetch/main.js
@@ -1,18 +1,22 @@
-import { TextLineStream, Semaphore } from './deps.js';
+import { TextLineStream, PromisePool } from './deps.js';
 
 const CONCURRENCY = parseInt(Deno.env.get('CONCURRENCY') || 10)
-// const REQUEST_TIMEOUT = parseInt(Deno.env.get('REQUEST_TIMEOUT') || 5)
-const LIMIT = parseInt(Deno.env.get('LIMIT') || 10000)
-// const ACCEPT_ENCODING = Deno.env.get('ACCEPT_ENCODING')
+const REQUEST_TIMEOUT = parseInt(Deno.env.get('REQUEST_TIMEOUT') || 5)
+const LIMIT = parseInt(Deno.env.get('LIMIT') || 1000)
+const ACCEPT_ENCODING = Deno.env.get('ACCEPT_ENCODING')
 
 const start = new Date()
 
 const headers = {
     'User-Agent': 'crawler-test',
+    ...(ACCEPT_ENCODING ? { 'Accept-Encoding': ACCEPT_ENCODING } : {})
 }
 
 console.log(`Starting crawl:`)
 console.log(` * CONCURRENCY: ${CONCURRENCY}`)
+console.log(` * REQUEST_TIMEOUT: ${REQUEST_TIMEOUT}`)
+console.log(` * LIMIT: ${LIMIT}`)
+console.log(` * ACCEPT_ENCODING: ${ACCEPT_ENCODING}`)
 
 
 const iterator = (async function* () {
@@ -40,6 +44,7 @@ const makeRequest = async url => {
         const signal = controller.signal;
         const timeout = setTimeout(() => controller.abort('timeout error'), 5000);
         const response = await fetch(url, { headers, signal });
+        await response.arrayBuffer();
         clearTimeout(timeout);
         const time = Date.now() - startTime
         console.log(`${url}: ${response.status} -- ${time}ms`)
@@ -53,32 +58,22 @@ const makeRequest = async url => {
     }
 }
 
-const semaphore = new Semaphore(CONCURRENCY);
+const { results, errors } = await PromisePool
+    .for(iterator)
+    .withConcurrency(CONCURRENCY)
+    .process(makeRequest)
 
-for await (const url of iterator) {
-    await semaphore.acquire();
-    (async () => {
-        await makeRequest(url);
-        semaphore.release();
-    })()
-}
+const aggregates = results.reduce((agg, result) => {
+    return { ...agg, [result.code]: (agg[result.code] || 0) + 1 }
+}, {})
+const avgTime = results.reduce((agg, result) => {
+    return agg + result.time
+}, 0) / results.length
+const medianTime = results.map(r => r.time).sort()[Math.floor(results.length / 2)]
 
-// const { results, errors } = await PromisePool
-//     .for(iterator)
-//     .withConcurrency(CONCURRENCY)
-//     .process(makeRequest)
-//
-// const aggregates = results.reduce((agg, result) => {
-//     return { ...agg, [result.code]: (agg[result.code] || 0) + 1 }
-// }, {})
-// const avgTime = results.reduce((agg, result) => {
-//     return agg + result.time
-// }, 0) / results.length
-// const medianTime = results.map(r => r.time).sort()[Math.floor(results.length / 2)]
-//
-// console.log(`Total time: ${(Date.now() - start) / 1000}s`)
-// console.log(`Average time: ${avgTime}`)
-// console.log(`Median time: ${medianTime}`)
-// console.log(`Total URLs: ${Object.values(aggregates).reduce((agg, count) => agg + count, 0)}`)
-//
-// console.log(aggregates)
+console.log(`Total time: ${(Date.now() - start) / 1000}s`)
+console.log(`Average time: ${avgTime}`)
+console.log(`Median time: ${medianTime}`)
+console.log(`Total URLs: ${Object.values(aggregates).reduce((agg, count) => agg + count, 0)}`)
+
+console.log(aggregates)
