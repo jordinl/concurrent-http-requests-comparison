49c7dcc851af13009bb0ed2da728d8be4369564f
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-15 15:38:28 +0200

    [deno] group response statuses

--- deno/fetch/main.js
+++ deno/fetch/main.js
@@ -3,22 +3,18 @@ import { TextLineStream, PromisePool } from './deps.js';
 const CONCURRENCY = parseInt(Deno.env.get('CONCURRENCY') || 10)
 const REQUEST_TIMEOUT = parseInt(Deno.env.get('REQUEST_TIMEOUT') || 5)
 const LIMIT = parseInt(Deno.env.get('LIMIT') || 1000)
-const ACCEPT_ENCODING = Deno.env.get('ACCEPT_ENCODING')
 const dataDir = Deno.env.get('DATA_DIR') || './data'
 
 const start = new Date()
 
 const headers = {
     'User-Agent': 'crawler-test',
-    ...(ACCEPT_ENCODING ? { 'Accept-Encoding': ACCEPT_ENCODING } : {})
 }
 
 console.log(`Starting crawl:`)
 console.log(` * CONCURRENCY: ${CONCURRENCY}`)
 console.log(` * REQUEST_TIMEOUT: ${REQUEST_TIMEOUT}`)
 console.log(` * LIMIT: ${LIMIT}`)
-console.log(` * ACCEPT_ENCODING: ${ACCEPT_ENCODING}`)
-
 
 const iterator = (async function* () {
     const file = await Deno.open(`${dataDir}/urls.txt`)
@@ -46,35 +42,45 @@ const makeRequest = async url => {
         const signal = AbortSignal.timeout(5000);
         const response = await fetch(url, { headers, signal, client });
         const body = await response.text();
-        body.replaceAll("\u0000", "");
+        const bodyLength = body.length;
         const time = Date.now() - startTime
-        console.log(`${url}: ${response.status} -- ${time}ms`)
-        return { code: response.status, time }
+        const color = response.ok ? "green" : "yellow"
+        console.log(`%c[${response.status}] ${url}: ${time}ms / Length: ${bodyLength}`, `color: ${color}`);
+        const code = `${response.status.toString()[0]}xx`;
+        return { code, time }
     } catch (error) {
         const time = Date.now() - startTime
         const messageParts = (error.message || error).split(": ").filter(u => !u.match(/https?:\/\//))
-        const code = messageParts[1] || messageParts[0]
-        console.error(`${url}: ${code} -- ${time}ms`)
-        return { code, time }
+        const exName = messageParts[1] || messageParts[0]
+        console.error(`%c[${exName}] ${url}: ${time}ms`, "color: red")
+        return { code: 'Exception', time }
     }
 }
 
-const { results, errors } = await PromisePool
+const { results } = await PromisePool
     .for(iterator)
     .withConcurrency(CONCURRENCY)
     .process(makeRequest)
 
+const defaultAggregates = {
+  "2xx": 0,
+  "3xx": 0,
+  "4xx": 0,
+  "5xx": 0,
+  "Exception": 0
+};
+
 const aggregates = results.reduce((agg, result) => {
-    return { ...agg, [result.code]: (agg[result.code] || 0) + 1 }
-}, {})
-const avgTime = results.reduce((agg, result) => {
-    return agg + result.time
-}, 0) / results.length
-const medianTime = results.map(r => r.time).sort()[Math.floor(results.length / 2)]
+    return { ...agg, [result.code]: agg[result.code] + 1 }
+}, defaultAggregates)
+const times = results.map(r => r.time);
+const avgTime = Math.round(times.reduce((agg, time) => agg + time, 0) / results.length);
+const medianTime = times.sort()[Math.floor(times.length / 2)]
+const maxTime = Math.max(...times);
 
 console.log(`Total time: ${(Date.now() - start) / 1000}s`)
-console.log(`Average time: ${avgTime}`)
-console.log(`Median time: ${medianTime}`)
+console.log(`Average time: ${avgTime}ms`)
+console.log(`Median time: ${medianTime}ms`)
+console.log(`Max time: ${maxTime}ms`)
 console.log(`Total URLs: ${Object.values(aggregates).reduce((agg, count) => agg + count, 0)}`)
-
 console.log(aggregates)
