fed59aa8cd83b9ab81f7d7b7140d93995f9f4aa7
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-16 13:00:49 +0200

    [deno] process inputs/outputs separately

--- deno/fetch/deps.js
+++ deno/fetch/deps.js
@@ -1,2 +1,2 @@
-export { TextLineStream } from "https://deno.land/std@0.224.0/streams/text_line_stream.ts";
-export { PromisePool } from "npm:@supercharge/promise-pool@3.2.0";
+export {TextLineStream} from "https://deno.land/std@0.224.0/streams/text_line_stream.ts";
+export {PromisePool} from "npm:@supercharge/promise-pool@3.2.0";
--- deno/fetch/main.js
+++ deno/fetch/main.js
@@ -1,86 +1,42 @@
-import { TextLineStream, PromisePool } from './deps.js';
+import {TextLineStream, PromisePool} from './deps.js';
 
-const CONCURRENCY = parseInt(Deno.env.get('CONCURRENCY') || 10)
-const REQUEST_TIMEOUT = parseInt(Deno.env.get('REQUEST_TIMEOUT') || 5)
-const LIMIT = parseInt(Deno.env.get('LIMIT') || 1000)
-const dataDir = Deno.env.get('DATA_DIR') || './data'
-
-const start = new Date()
-
+const CONCURRENCY = parseInt(Deno.env.get('CONCURRENCY') || 10);
+const timeout = parseInt(Deno.env.get('REQUEST_TIMEOUT') || 5) * 1000;
 const headers = {
-    'User-Agent': 'crawler-test',
+  'User-Agent': Deno.env.get('USER_AGENT') || 'deno-fetch',
 }
 
-console.log(`Starting crawl:`)
-console.log(` * CONCURRENCY: ${CONCURRENCY}`)
-console.log(` * REQUEST_TIMEOUT: ${REQUEST_TIMEOUT}`)
-console.log(` * LIMIT: ${LIMIT}`)
+const urls = (async function* () {
+  const readable = Deno.stdin.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
 
-const iterator = (async function* () {
-    const file = await Deno.open(`${dataDir}/urls.txt`)
-
-    const readable = file.readable
-        .pipeThrough(new TextDecoderStream()) // decode Uint8Array to string
-        .pipeThrough(new TextLineStream()) // split string line by line
-
-    let count = 0
-    for await (const line of readable) {
-        yield line
-        count++
-        if (count >= LIMIT) {
-            return
-        }
-    }
+  for await (const line of readable) {
+    yield line;
+  }
 })();
 
 const client = Deno.createHttpClient({poolMaxIdlePerHost: 0});
 
 const makeRequest = async url => {
-    const startTime = Date.now()
+  const startTime = new Date();
+  let code;
+  let bodyLength = 0;
 
-    try {
-        const signal = AbortSignal.timeout(5000);
-        const response = await fetch(url, { headers, signal, client });
-        const body = await response.text();
-        const bodyLength = body.length;
-        const time = Date.now() - startTime
-        const color = response.ok ? "green" : "yellow"
-        console.log(`%c[${response.status}] ${url}: ${time}ms / Length: ${bodyLength}`, `color: ${color}`);
-        const code = `${response.status.toString()[0]}xx`;
-        return { code, time }
-    } catch (error) {
-        const time = Date.now() - startTime
-        const messageParts = (error.message || error).split(": ").filter(u => !u.match(/https?:\/\//))
-        const exName = messageParts[1] || messageParts[0]
-        console.error(`%c[${exName}] ${url}: ${time}ms`, "color: red")
-        return { code: 'Exception', time }
-    }
+  try {
+    const signal = AbortSignal.timeout(timeout);
+    const response = await fetch(url, {headers, signal, client})
+    const body = await response.text();
+    code = response.status;
+    bodyLength = body.length;
+  } catch (error) {
+    code = error.cause?.code || error.name;
+  }
+  const duration = new Date() - startTime;
+  console.log(`${url},${code},${startTime.toISOString()},${duration},${bodyLength}`);
 }
 
-const { results } = await PromisePool
-    .for(iterator)
-    .withConcurrency(CONCURRENCY)
-    .process(makeRequest)
-
-const defaultAggregates = {
-  "2xx": 0,
-  "3xx": 0,
-  "4xx": 0,
-  "5xx": 0,
-  "Exception": 0
-};
-
-const aggregates = results.reduce((agg, result) => {
-    return { ...agg, [result.code]: agg[result.code] + 1 }
-}, defaultAggregates)
-const times = results.map(r => r.time);
-const avgTime = Math.round(times.reduce((agg, time) => agg + time, 0) / results.length);
-const medianTime = times.sort()[Math.floor(times.length / 2)]
-const maxTime = Math.max(...times);
-
-console.log(`Total time: ${(Date.now() - start) / 1000}s`)
-console.log(`Average time: ${avgTime}ms`)
-console.log(`Median time: ${medianTime}ms`)
-console.log(`Max time: ${maxTime}ms`)
-console.log(`Total URLs: ${Object.values(aggregates).reduce((agg, count) => agg + count, 0)}`)
-console.log(aggregates)
+await PromisePool
+  .for(urls)
+  .withConcurrency(CONCURRENCY)
+  .process(makeRequest);
