ff9b3887746bccbe256a83f21439c004feac9438
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-16 09:43:23 +0200

    [node-fetch] process inputs/outputs separately

--- node/fetch/main.js
+++ node/fetch/main.js
@@ -1,72 +1,33 @@
-import fs from 'fs';
+import {createInterface} from "node:readline";
 import {PromisePool} from '@supercharge/promise-pool'
 
 const CONCURRENCY = parseInt(process.env.CONCURRENCY || 10)
 const REQUEST_TIMEOUT = parseInt(process.env.REQUEST_TIMEOUT || 5)
-const LIMIT = parseInt(process.env.LIMIT || 1000)
-const start = new Date()
-const dataDir = process.env.DATA_DIR || './data'
-
-console.log(`Starting crawl:`)
-console.log(` * CONCURRENCY: ${CONCURRENCY}`)
-console.log(` * REQUEST_TIMEOUT: ${REQUEST_TIMEOUT}`)
-console.log(` * LIMIT: ${LIMIT}`)
 
 const headers = {
-    'User-Agent': 'crawler-test',
-    'Accept-Encoding': 'gzip, deflate, br'
+  'User-Agent': 'crawler-test',
+  'Accept-Encoding': 'gzip, deflate, br'
 }
-
-const iterator = (async function* () {
-    const readableStream = fs.createReadStream(`${dataDir}/urls.txt`, {encoding: 'utf8'})
-
-    let count = 0
-    let last = ''
-    for await (const chunk of readableStream) {
-        const lines = (last + chunk).split('\n')
-        last = lines.pop()
-        for (const line of lines) {
-            yield line
-            count++
-            if (count >= LIMIT) {
-                return
-            }
-        }
-    }
-})();
 
 const makeRequest = async url => {
-    const startTime = Date.now()
+  const startTime = new Date();
+  let code;
+  let bodyLength = 0;
 
-    try {
-        const response = await fetch(url, {headers, signal: AbortSignal.timeout(REQUEST_TIMEOUT * 1000)})
-        const time = Date.now() - startTime
-        console.log(`${url}: ${response.status} -- ${time}ms`)
-        return {code: response.status, time}
-    } catch (error) {
-        const time = Date.now() - startTime
-        const code = error.cause?.code || error.name
-        console.error(`${url}: ${code} -- ${time}ms`)
-        return {code, time}
-    }
+  try {
+    const signal = AbortSignal.timeout(REQUEST_TIMEOUT * 1000);
+    const response = await fetch(url, {headers, signal})
+    const body = await response.text();
+    code = response.status;
+    bodyLength = body.length;
+  } catch (error) {
+    code = error.cause?.code || error.name;
+  }
+  const duration = new Date() - startTime;
+  console.log(`${url},${code},${startTime.toISOString()},${duration},${bodyLength}`);
 }
 
-const {results, errors} = await PromisePool
-    .for(iterator)
-    .withConcurrency(CONCURRENCY)
-    .process(makeRequest)
-
-const aggregates = results.reduce((agg, result) => {
-    return {...agg, [result.code]: (agg[result.code] || 0) + 1}
-}, {})
-const avgTime = results.reduce((agg, result) => {
-    return agg + result.time
-}, 0) / results.length
-const medianTime = results.map(r => r.time).sort()[Math.floor(results.length / 2)]
-
-console.log(`Total time: ${(Date.now() - start) / 1000}s`)
-console.log(`Average time: ${avgTime}`)
-console.log(`Median time: ${medianTime}`)
-console.log(`Total URLs: ${Object.values(aggregates).reduce((agg, count) => agg + count, 0)}`)
-
-console.log(aggregates)
+await PromisePool
+  .for(createInterface({input: process.stdin}))
+  .withConcurrency(CONCURRENCY)
+  .process(makeRequest)
---
+++ scripts/process-output.js
@@ -0,0 +1,48 @@
+import {createInterface} from "node:readline";
+
+const colors = {
+  "red": "\x1b[31m",
+  "green": "\x1b[32m",
+  "yellow": "\x1b[33m",
+  "white": "\x1b[37m",
+  "reset": "\x1b[0m",
+}
+
+const print = (message, color = "white") => {
+  console.log(`${colors[color]}${message}${colors.reset}`);
+}
+
+let results = []
+
+for await (const line of createInterface({input: process.stdin})) {
+  const [url, code, startTimeStr, durationStr, length] = line.split(",");
+  const startTime = Date.parse(startTimeStr);
+  const duration = parseInt(durationStr);
+  const color = code[0] === "2" ? "green" : (code.match(/^[0-9]{3}$/) ? "yellow" : "red");
+  print(`[${code}] ${url}: ${duration}ms`, color);
+
+  results.push({code, startTime, duration});
+}
+
+const defaultCounts = ['2xx', '3xx', '4xx', '5xx', 'Exception'].reduce((agg, key) => {
+  return {...agg, [key]: 0}
+}, {});
+
+const counts = results.reduce((agg, {code}) => {
+  const shortCode = code.match(/^[0-9]{3}$/) ? `${code[0]}xx` : 'Exception';
+  return {...agg, [shortCode]: agg[shortCode] + 1};
+}, defaultCounts);
+const avgDuration = results.reduce((agg, r) => agg + r.duration, 0) / results.length;
+const startTime = Math.min(...results.map(r => r.startTime));
+const endTime = Math.max(...results.map(r => r.startTime + r.duration));
+const totalTime = endTime - startTime;
+
+const aggregates = {
+  totalTime,
+  avgDuration: Math.round(avgDuration),
+  totalUrls: Object.values(counts).reduce((agg, count) => agg + count, 0),
+  okReqsSecond: Math.round(counts['2xx'] / totalTime * 1000),
+  ...counts
+}
+
+console.log(aggregates);
