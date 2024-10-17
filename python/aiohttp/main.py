55cc066de915b336fe7d386a64e1f1a44a5af970
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-17 12:39:47 +0200

    [python-aiohttp] process inputs/outputs separately

--- python/aiohttp/main.py
+++ python/aiohttp/main.py
@@ -2,32 +2,17 @@ import os
 import sys
 import asyncio
 import aiohttp
-from time import time
+from datetime import datetime
 
 CONCURRENCY = int(os.environ.get('CONCURRENCY', 10))
 REQUEST_TIMEOUT = int(os.environ.get('REQUEST_TIMEOUT', 5))
-URL_LIMIT = int(os.environ.get('LIMIT', 1000))
-
-def file_reader():
-    count = 0
-    with open('/mnt/appdata/urls.txt', 'r') as file:
-        for line in file:
-            yield line.strip()
-            count += 1
-            if count >= URL_LIMIT:
-                break
-
-print(f"Starting crawl")
-print(f" * CONCURRENCY: {CONCURRENCY}")
-print(f" * REQUEST_TIMEOUT: {REQUEST_TIMEOUT}s")
-print(f" * URL_LIMIT: {URL_LIMIT}")
+USER_AGENT = os.environ.get('USER_AGENT', 'python-aiohttp')
 
 headers = {
-    'User-Agent': 'crawler-test',
+    'User-Agent': USER_AGENT,
     'Accept-Encoding': 'gzip, deflate, br'
 }
 
-conn = aiohttp.TCPConnector(limit=CONCURRENCY)
 semaphore = asyncio.Semaphore(CONCURRENCY)
 
 def get_exception(exc):
@@ -37,45 +22,25 @@ def get_exception(exc):
 
 async def make_request(url):
     async with semaphore:
-        start = time()
+        start = datetime.now()
+        body_length = 0
 
         timeout=aiohttp.ClientTimeout(total=REQUEST_TIMEOUT)
 
         async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
             try:
                 async with session.get(url) as response:
+                    body = await response.text()
+                    body_length = len(body)
                     code = response.status
             except Exception as exc:
                 code = get_exception(exc)
             finally:
-                duration = time() - start
-                print(f"{url} - {code} - {duration:.2f}s")
-                return {'url': url, 'code': code, 'time': duration}
+                duration = round((datetime.now() - start).total_seconds() * 1000)
+                print(f"{url},{code},{start.isoformat()},{duration},{body_length}")
 
 async def main():
-    urls = file_reader()
-    tasks = [make_request(url) for url in urls]
+    tasks = [make_request(url.strip()) for url in sys.stdin]
+    await asyncio.gather(*tasks)
 
-    results = await asyncio.gather(*tasks)
-
-    aggregates = {}
-    for result in results:
-        code = result['code']
-        aggregates[code] = aggregates.get(code, 0) + 1
-
-    avg_time = sum(result['time'] for result in results) / len(results)
-    median_time = sorted(result['time'] for result in results)[len(results) // 2]
-
-    print(f"Average time: {avg_time * 1000:.2f}ms")
-    print(f"Median time: {median_time * 1000:.2f}ms")
-    print(f"Total URLs: {sum(aggregates.values())}")
-
-    print(aggregates)
-
-try:
-    start = time()
-    asyncio.run(main())
-except Exception as exc:
-    print(exc)
-finally:
-    print(f"Total time: {time() - start:.2f}s")
+asyncio.run(main())
