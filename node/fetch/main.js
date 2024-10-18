f09565adf4b975a990d33420d0d773d523fcf5a0
Jordi Noguera <jordinoguera83@gmail.com>
2024-10-18 16:02:08 +0200

    [node-fetch] use USER_AGENT

--- node/fetch/main.js
+++ node/fetch/main.js
@@ -1,13 +1,12 @@
 import {createInterface} from "node:readline";
-import {PromisePool} from '@supercharge/promise-pool'
+import {PromisePool} from "@supercharge/promise-pool";
 
-const CONCURRENCY = parseInt(process.env.CONCURRENCY || 10)
-const REQUEST_TIMEOUT = parseInt(process.env.REQUEST_TIMEOUT || 5)
-
+const CONCURRENCY = parseInt(process.env.CONCURRENCY || 10);
+const REQUEST_TIMEOUT = parseInt(process.env.REQUEST_TIMEOUT || 5);
 const headers = {
-  'User-Agent': 'crawler-test',
-  'Accept-Encoding': 'gzip, deflate, br'
-}
+  "User-Agent": process.env.USER_AGENT || "node-fetch",
+  "Accept-Encoding": "gzip, deflate, br"
+};
 
 const makeRequest = async url => {
   const startTime = new Date();
@@ -16,7 +15,7 @@ const makeRequest = async url => {
 
   try {
     const signal = AbortSignal.timeout(REQUEST_TIMEOUT * 1000);
-    const response = await fetch(url, {headers, signal})
+    const response = await fetch(url, {headers, signal});
     const body = await response.text();
     code = response.status;
     bodyLength = body.length;
@@ -25,9 +24,9 @@ const makeRequest = async url => {
   }
   const duration = new Date() - startTime;
   console.log(`${url},${code},${startTime.toISOString()},${duration},${bodyLength}`);
-}
+};
 
 await PromisePool
   .for(createInterface({input: process.stdin}))
   .withConcurrency(CONCURRENCY)
-  .process(makeRequest)
+  .process(makeRequest);
